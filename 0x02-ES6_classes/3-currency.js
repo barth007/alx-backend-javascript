2971cd221d798cbd7556e66c9ad5c0a0991c9943
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-09-30 07:09:18 +0000

    added task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,11 +1,5 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
-    }
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._code = code;
     this._name = name;
   }
