61d8c32ed09e05f6ef171ce8d70fdf2f81926f61
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-01 04:42:27 +0000

    added task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError(' Name must be a string ');
+    }
+    this._name = value;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError(' Code must be a string ');
+    }
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
