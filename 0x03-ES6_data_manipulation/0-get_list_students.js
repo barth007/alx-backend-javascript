87eddd567a9b79de3635c5a986935012dc24b5c8
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-04 04:04:28 +0000

    added task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const info = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 3, firstname: 'Serena', location: 'San Francisco' },
+  ];
+  return info;
+}
