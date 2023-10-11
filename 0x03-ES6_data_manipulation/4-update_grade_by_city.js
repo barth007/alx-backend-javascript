53868950203d2509ef8c908a34f8105b1541dda9
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-11 14:45:05 +0000

    task 4 done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,12 @@
 export default function updateStudentGradeByCity(listStudent, city, newGrades) {
-  const mappedArray = listStudent.map((element) =>{
+  const filtered = listStudent.filter((cities) => cities.location === city);
+  const mappedArray = filtered.map((element) => {
+    const matchedValue = newGrades.find(({ studentId }) => studentId === element.id);
+    if (matchedValue) {
+      return { ...element, grade: matchedValue.grade };
+    }
+
+    return { ...element, grade: 'N/A' };
   });
+  return mappedArray;
 }
