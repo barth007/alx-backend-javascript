0a3fc77c6511e461a938c0a23ee01cc54f59546e
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-26 04:32:42 +0000

    added task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch((error) => {
+      console.log('Signup system offline', error);
+    });
+}
