636d5363b06dd14f7e44b8c9a0500157910f20a1
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-26 09:38:44 +0000

    updating code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const result = await Promise.all([
-      uploadPhoto(fileName),
-      signUpUser(firstName, lastName),
-    ]);
-    console.log(result);
-  } catch (_) {
-    /* eslint-disable-line no-empty */
-  }
+  const results = await Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]);
+  results.forEach((result) => ({
+    status: result.status,
+    value: result.status === 'fulfiled' ? result.value : result.reason,
+
+  }));
 }
