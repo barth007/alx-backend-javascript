482c654e0854ac512424f1938ba8159f11097609
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-26 08:01:17 +0000

    updated task 2 and 5

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((response) => {
-    /* eslint-disable no-unused-vars */
-    const inactiveResponse = response;
-    /* eslint-enable no-unused-vars */
+  promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }).catch((error) => {
-    /* eslint-disable no-unused-vars */
-    const inactiveError = error;
-    /* eslint-enable no-unused-vars */
-    return new Error();
-  });
+  }).catch(() => new Error());
 }
--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,9 @@
-export default function uploadPhoto(fileName) {
-  return new Promise((resolve, reject) => {
-    reject(new Error(`${fileName} cannot be processed`));
-  });
+//export default function uploadPhoto(fileName) {
+  //return new Promise((_, reject) => {
+   // reject(new Error(`${fileName} cannot be processed`));
+  //});
+//}
+
+export default function uploadPhotw(fileName){
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
