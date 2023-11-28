044e2ce3a1dc39c1c0c0306b28697d1a1fb45f1f
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-28 02:20:08 +0000

    updated task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,3 +1,27 @@
-const countStudents = require('./2-read_file');
+const fs = require('fs');
+const csv = require('csv-parser');
 
-countStudents("database.csv");
+function countStudents(path) {
+  const result = [];
+  fs.createReadStream(path)
+    .pipe(csv())
+    .on('data', (data) => result.push(data))
+    .on('end', () => {
+      const numberOfStudents = result.length;
+      console.log(`Number of students: ${numberOfStudents}`);
+      const { csFields, sweFields } = result.reduce((accumilator, currentValue) => {
+        if (currentValue.field === 'CS') {
+          accumilator.csFields.push(currentValue.firstname);
+        } else {
+          accumilator.sweFields.push(currentValue.firstname);
+        }
+        return accumilator;
+      },
+      { csFields: [], sweFields: [] });
+
+      console.log(`Number of students in CS: ${csFields.length}. List: ${csFields}`);
+      console.log(`Number of students in SWE: ${sweFields.length}. List: ${sweFields}`);
+    });
+}
+
+module.exports = countStudents;
