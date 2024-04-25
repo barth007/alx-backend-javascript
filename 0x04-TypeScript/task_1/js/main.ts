42f342ca58b60350576a1a0d20b8681b098e52d4
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2024-04-25 16:25:46 +0100

    added task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -33,9 +33,38 @@ interface printTeacherFunction{
 }
 
 const printTeacher: printTeacherFunction =(firstName, lastName) =>{
-    const name = `${firstName[0]}.${lastName}`;
+    const name = `${firstName.charAt(0)}.${lastName}`;
     return name;
 }
 
 const printName = printTeacher("Barth", "John");
 console.log(printName)
+
+
+interface StudentClassConstructor{
+    new(firstName:string, lastName: string): StudentClass,
+}
+interface StudentClassInterface {
+    workOnHomework(): string,
+    displayName(): string,
+
+}
+
+class StudentClass implements StudentClassInterface{
+    private firstName: string;
+    private  lastName: string;
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+    workOnHomework(): string {
+        return "Currently working"
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const instance: StudentClass = new StudentClass("Barth", "Bassey");
+console.log(instance.displayName());
